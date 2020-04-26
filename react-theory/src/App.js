65277a45a2fac18365c0068106c196ad219ff71a
NikolayB7 <biletskiyna@gmail.com>
2020-04-26 23:18:49 +0300

    jsx
    ограничения jsx

---
+++ react-theory/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="title">Hello World!</h1>
+
+    </div>
+  );
+  // return React.createElement(
+  //   'div',
+  //   { className: 'App' },
+  //   React.createElement(
+  //     'h1',
+  //     { className: 'title' },
+  //     'Hello World'
+  //   )
+
+  // )
+}
+
+export default App;
